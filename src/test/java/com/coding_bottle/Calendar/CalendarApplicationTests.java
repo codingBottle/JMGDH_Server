cf61c7bbce6eac0ee7c801b4390e08f6b7f3a13e
JaeYong <yjy850146@gmail.com>
2023-11-11 02:08:35 +0900

    [Feat] CI_CD, CalendarApplicationTests 파일 수정
    - pull-request 이벤트 트리거 없앰
    - @SpringBootTest 비활성화

--- src/test/java/com/coding_bottle/Calendar/CalendarApplicationTests.java
+++ src/test/java/com/coding_bottle/Calendar/CalendarApplicationTests.java
@@ -3,10 +3,10 @@ package com.coding_bottle.Calendar;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class CalendarApplicationTests {
 
-	@Test
+	// @Test
 	void contextLoads() {
 	}
 
