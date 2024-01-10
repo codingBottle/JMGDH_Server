d01da8a0a3d648e56afaa27d82d160c23ad2a76a
vact19 <chj6703@gmail.com>
2024-01-10 23:49:46 +0900

    Feat. 일정 반복등록기능 추가

--- src/main/java/com/codingbottle/calendar/domain/daterepeater/RepeatInterval.java
+++ src/main/java/com/codingbottle/calendar/domain/daterepeater/RepeatInterval.java
@@ -1,6 +1,11 @@
 package com.codingbottle.calendar.domain.daterepeater;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum RepeatInterval {
-    EVERY_WEEK, EVERY_TWO_WEEKS, EVERY_FOUR_WEEKS
+    EVERY_WEEK("매주마다"), EVERY_TWO_WEEKS("격주마다")
+    , EVERY_FOUR_WEEKS("4주마다");
     ;
+    public final String description;
 }
--- src/main/java/com/codingbottle/calendar/domain/schedule/dto/ScheduleCreateReqDto.java
+++ src/main/java/com/codingbottle/calendar/domain/schedule/dto/ScheduleCreateReqDto.java
@@ -1,5 +1,6 @@
 package com.codingbottle.calendar.domain.schedule.dto;
 
+import com.codingbottle.calendar.domain.daterepeater.RepeatInterval;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 
@@ -13,12 +14,16 @@ public record ScheduleCreateReqDto(
         String title,
         @JsonFormat(pattern = "yyyy-MM-dd", shape = Shape.STRING)
         @NotNull
-        LocalDate date,
+        LocalDate targetDate,
         @NotNull(message = "종일 여부를 입력해주세요.")
         Boolean isAllDay,
         @JsonFormat(pattern = "HH:mm", shape = Shape.STRING)
         LocalTime startTime,
         @JsonFormat(pattern = "HH:mm", shape = Shape.STRING)
-        LocalTime endTime
+        LocalTime endTime,
+
+        // 반복주기 repeatInterval을 반복횟수 repeatCount만큼 반복한다.
+        RepeatInterval repeatInterval,
+        Integer repeatCount
 ) {
 }
