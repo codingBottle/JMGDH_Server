cd3122cf9829527306f82bd877a950d596f8eb8f
vact19 <chj6703@gmail.com>
2024-02-18 19:10:07 +0900

    Fix. 월별 캘린더 SQL 오류 수정
    조건절의 우선순위를 명확하게 하여 memberId 체크가 무시되는 버그 수정

--- src/main/java/com/codingbottle/calendar/domain/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/codingbottle/calendar/domain/schedule/repository/ScheduleRepository.java
@@ -17,9 +17,9 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     @Query("SELECT s FROM Schedule s" +
             " WHERE s.member.id = :memberId" +
             " AND" +
-            " (FUNCTION('YEAR', s.startDate) = :year AND FUNCTION('MONTH', s.startDate) = :month)" +
+            " ((FUNCTION('YEAR', s.startDate) = :year AND FUNCTION('MONTH', s.startDate) = :month)" +
             " OR " +
-            " (FUNCTION('YEAR', s.endDate) = :year AND FUNCTION('MONTH', s.endDate) = :month)")
+            " (FUNCTION('YEAR', s.endDate) = :year AND FUNCTION('MONTH', s.endDate) = :month))")
     List<Schedule> findByYearAndMonth(@Param("year") int year, @Param("month") int month, @Param("memberId") long memberId);
 
     @Query("SELECT s.id FROM Schedule s WHERE s.repeatCode = :repeatCode")
